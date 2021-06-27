3ae616f61be627a70f75986465650b8984f87c04
lchtao26 <lchtao26@icloud.com>
2021-06-27 23:33:17 +0800

    chore: 配置electron

---
+++ electron-starter.js
@@ -0,0 +1,24 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // and load the index.html of the app.
+  const startUrl = path.join(__dirname, "build/index.html");
+  mainWindow.loadFile(startUrl);
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+});
